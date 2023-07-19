043c2bf1dc3a5e7327b71a811959561ac64c590f
Pitta Andrew Blaze <113298007+andrew2719@users.noreply.github.com>
2023-07-19 07:34:39 +0000

    committed

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -19,8 +19,27 @@ class Lexical_analyzer():
     
     def __init__(self,text):
         self.text = text
+        
+    def print_table(key,op,ids,special,nums,single):
+        elements = {'keywords':list(key),'operators':list(op),'identifiers':list(ids),'special':list(special),'numbers':list(nums),'single':list(single)}
+        
+        # all the elements are not of same length so we need to make them of same length 
+        # so that we can make a dataframe out of it
+        max_len = 0
+        for i in elements:
+            if len(elements[i]) > max_len:
+                max_len = len(elements[i])
 
-    def analyze_c_program():
+
+        for i in elements:
+            while len(elements[i]) < max_len:
+                elements[i].append('')
+
+        # print(elements)
+        df = pd.DataFrame(elements)
+        print(df)
+
+    def analyze_c_program(self):
         #initializing the lists as [] for all the six symbols
         key,ids,op,special,nums,single = [[] for i in range(6)]
 
@@ -33,17 +52,23 @@ class Lexical_analyzer():
         escaped_single_operators = [re.escape(op) for op in all_elements.single_operators] # escape all the single operators by regex
         escaped_special_chars = [re.escape(special_char) for special_char in all_elements.special_chars] # escape all the special characters by regex
 
+
         # join all the escaped operators by |, the formed string for example ++|--|+=|-=|*=|/=|%=|==|!=|<=|>=|&&|\\|\\|
         operators_re = '|'.join(escaped_operators)
+
 
         # join all the escaped single operators by |, the formed string for example +|-|*|/|%|=|<|>|!|&|||^|~|?|: 
         single_operators_re = '|'.join(escaped_single_operators)
 
+
         # join all the escaped special characters by |, the formed string for example (|)|{|}|[|]|,|;
         special_re = '|'.join(escaped_special_chars)
 
+
         #tokenize the content with regex findall
         tokens = re.findall(r'\b\w+\b'+'|'+operators_re+'|'+single_operators_re+'|'+special_re, content)
+
+        print(tokens)
 
         for token in tokens:
             if token in all_elements.keywords:
@@ -67,4 +92,19 @@ class Lexical_analyzer():
                 # print(token ,"-> number")
                 nums.append(token)
 
+
         key,ids,op,special,nums,single = set(key),set(ids),set(op),set(special),set(nums),set(single)
+        
+        Lexical_analyzer.print_table(key,op,ids,special,nums,single)
+    
+
+
+def convert_file_to_content(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+text = convert_file_to_content('test.txt')
+
+la  = Lexical_analyzer(text)
+la.analyze_c_program()
